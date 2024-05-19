306c12e34810a32a931397576b523a7008f42406
firyalhfz <iyaalfiryal@gmail.com>
2024-05-19 20:30:56 +0700

    home, about, projects

---
+++ data/index.ts
@@ -0,0 +1,269 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "With 2 years of experiences",
+    description: "Developing dynamic and seamless Website",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "EventBro APMF 2022",
+    des: "Admin Dashboard for managing the APMF 2022 event, including scheduling, sponsors, banners, speakers, exhibitors, and more. Designed to streamline event logistics, it ensures efficient coordination and a successful event experience",
+    img: "/eventbroreal.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/js.png",
+      "/antdesign.svg",
+      "/gitlab.png",
+    ],
+    link: "https://github.com/adrianhajdin?tab=repositories",
+    place: "Kodingworks",
+  },
+  {
+    id: 2,
+    title: "Dashboard Admin HaiDafam",
+    des: "A comprehensive tool for managing hotel operations, withdrawal transactions, and top-up activities. Designed for efficiency, it enables administrators to monitor and optimize hotel processes seamlessly.",
+    img: "/haidafamreal.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/antdesign.svg",
+      "/gitlab.png",
+    ],
+    link: "https://github.com/adrianhajdin/zoom-clone",
+    place: "Pesan.io",
+  },
+  {
+    id: 3,
+    title: "Dashboard Admin Haifood",
+    des: "A platform for managing food delivery operations, including promotions, food categories, food products, restaurants, and more. Designed to enhance efficiency, it allows administrators to oversee and optimize the entire food delivery process seamlessly",
+    img: "/haifoodreal.jpg",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/antdesign.svg",
+      "/gitlab.png",
+    ],
+    link: "https://github.com/adrianhajdin/ai_saas_app",
+    place: "Haitoko",
+  },
+  {
+    id: 5,
+    title: "Laundry Ciloa",
+    des: "Landing page that provides comprehensive information about laundry services, step-by-step guide on how to place an order. Designed for convenience, it allows customers to effortlessly access service details, pricing, and order their laundry services online with ease",
+    img: "/laundryciloa.png",
+    iconLists: ["/next.svg", "/tail.svg", "/js.png", "/git.svg", "/c.svg"],
+    link: "https://github.com/adrianhajdin/ai_saas_app",
+    place: "Freelance",
+  },
+  {
+    id: 4,
+    title: "Umroh.in",
+    des: "A user-friendly platform showcasing Umrah packages, guiding Ustadz profiles, promotions, and an easy-to-use Umrah booking system. Designed to provide comprehensive information and simplify the booking process for a seamless Umrah experience",
+    img: "/umrohin.svg",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/antdesign.svg",
+      "/git.svg",
+    ],
+    link: "https://github.com/adrianhajdin/iphone",
+    place: "umroh.in",
+  },
+  {
+    id: 5,
+    title: "Teman Cuan",
+    des: "Teman Cuan Admin Dashboard: A specialized tool for managing a social network website, designed to foster genuine interactions and relationships among users. It enables administrators to efficiently oversee user activities, content, and engagement to ensure a thriving online community",
+    img: "/temancuanreal.jpg",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/antdesign.svg",
+      "/gitlab.png",
+    ],
+    link: "https://github.com/adrianhajdin/iphone",
+    place: "TemanCuan",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
