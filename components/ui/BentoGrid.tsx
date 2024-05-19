34aad81c461b807feab0b93d0430584e366d821f
firyalhfz <iyaalfiryal@gmail.com>
2024-05-19 21:44:49 +0700

    refactor ui about and ui projects

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -62,7 +62,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "firyalhuwaidaa@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
--- data/index.ts
+++ data/index.ts
@@ -12,8 +12,8 @@ export const gridItems = [
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
+    titleClassName: "justify-start",
+    img: "/lap.png",
     spareImg: "",
   },
   {
